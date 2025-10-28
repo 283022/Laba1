05227a133c5f56eb37765ea924bcea0080be0bcf
Artem <bafara032@gmail.com>
2025-10-28 23:02:47 +0300

    update. just write some test for this

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,13 +6,11 @@ check.AddElement(1,2);
 check.AddElement(2,3); 
 check.AddElement(3,4);
 
-/*
 IEnumerator check2 = check.GetEnumerator();
 var s = check2.Current;
 
 var check3 = check.GetEnumerator();
 var s2 = check3.Current;
-*/
 
 
 foreach (var item in check)
