4450c220c38cc0664cad571ff5ea6b082567c4f2
Artem <bafara032@gmail.com>
2025-10-28 23:05:41 +0300

    do all

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,16 +6,17 @@ check.AddElement(1,2);
 check.AddElement(2,3); 
 check.AddElement(3,4);
 
+/*
 IEnumerator check2 = check.GetEnumerator();
 var s = check2.Current;
 
 var check3 = check.GetEnumerator();
 var s2 = check3.Current;
+*/
 
-//комментарий
 foreach (var item in check)
 {
-    Console.WriteLine(item.GetType());
+    Console.WriteLine(item);
 }
 
 
